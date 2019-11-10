f0d165ebab6946152ebca4f65e0730f9c0684d22
ferchuqui <luisfernandofarji@hotmail.com>
2019-11-10 16:18:40 -0300

    Making DNI

--- component/Form.js
+++ component/Form.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+  state = { name: '' }
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(this.state.name)
+    console.log(this.state.DNI)
+  }
+  handleNameChange = (e) => {
+    this.setState({name: e.target.value})
+  }
+  handleDNIChange = (e) => {
+    this.setState({DNI: e.target.value})
+  }
   render() {
     return (
-      <form><h1>Reserva tu turno</h1>
-      <h2>Completa tus datos y nosotros te avisaremos</h2>
-        <input type="text" name="name" placeholder="Enter your Name" />
-        <input type="number" name="dni" placeholder="Enter your DNI" />
-
+      <form onSubmit={this.handleSubmit}>
+        <h1>Reserva tu turno</h1>
+        <h2>Completa tus datos y nosotros te avisaremos</h2>
+        <input type="text" name="name" value={this.state.name} onChange={this.handleNameChange} placeholder="Enter your Name" />
+        <input type="number" name="dni" value={this.state.dni} onChange={this.handleDNIChange}placeholder="Enter your DNI" />
         <input type="submit" />
         <style jsx>{`
         

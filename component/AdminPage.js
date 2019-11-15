e35c60ee98587a246c6568f387cb8997c7c3bdce
ferchuqui <luisfernandofarji@hotmail.com>
2019-11-15 15:34:22 -0300

    Added control in admin page

--- component/AdminPage.js
+++ component/AdminPage.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react'
 
 class AdminPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      turns: []
+    };
+  }
+  componentDidMount() {
+    setInterval(() => {
+      fetch("https://turner-api.herokuapp.com/turns")
+        .then(response => {
+          return response.json();
+        })
+        .then(body => {
+          this.setState({ turns: body });
+        });
+    }, 1000);
+  }
+  handleClick=(id)=>{
+    fetch(`https://turner-api.herokuapp.com/turns/${id}/complete`, {
+      method: "put"
+    })
+
+  }
   render () {
     return (
       <div>
-      Hola Mundo
+  <ul>
+    {this.state.turns.map((turn)=>{
+      return <li key={turn.id}>{turn.id} <a href="#" onClick={()=>{
+        this.handleClick(turn.id)
+      }}>Completado</a></li>
+    })}
+  </ul>
+      
       </div>
     )
   }

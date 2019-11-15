690419e3e9d22ef2b4cc4961f06a42d4ff26c97d
ferchuqui <luisfernandofarji@hotmail.com>
2019-11-15 15:02:25 -0300

    added json

--- component/BigNumber.js
+++ component/BigNumber.js
@@ -1,29 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class BigNumber extends Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       number: 18
-    }
+    };
   }
-  render () {
-    return (
-      <div>{this.state.number}
-      <style jsx>{`
+  componentDidMount() {
+    setInterval(() => {
+      fetch("https://turner-api.herokuapp.com/turns")
+        .then(response => {
+          return response.json();
+        })
+        .then(body => {
+          this.setState({ number: body[0].id });
+        });
+    }, 1000);
+  }
 
-        div {
-          height: 100vh;
-          background: red;
-          font-size: 18rem;
-          text-align: center;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-        }
-      `}</style>
+  render() {
+    return (
+      <div>
+        {this.state.number}
+        <style jsx>{`
+          div {
+            height: 100vh;
+            background: red;
+            font-size: 18rem;
+            text-align: center;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+        `}</style>
       </div>
-    )
+    );
   }
 }
-export default BigNumber
+export default BigNumber;
